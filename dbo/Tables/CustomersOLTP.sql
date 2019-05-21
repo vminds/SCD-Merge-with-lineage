e268f428143772983357cc4ed4e9c689755d6da1
Hans Vetters <hans@vminds.be>
2019-05-21 11:25:51 +0200

    init customer table + data load

---
+++ dbo/Tables/CustomersOLTP.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE [dbo].[CustomersOLTP](
+	[CustomerID] [int] NOT NULL,
+	[CustomerName] [nvarchar](100) NOT NULL,
+	[BillToCustomerID] [int] NOT NULL,
+	[CustomerCategoryID] [int] NOT NULL,
+	[BuyingGroupID] [int] NULL,
+	[PrimaryContactPersonID] [int] NOT NULL,
+	[AlternateContactPersonID] [int] NULL,
+	[DeliveryMethodID] [int] NOT NULL,
+	[DeliveryCityID] [int] NOT NULL,
+	[PostalCityID] [int] NOT NULL,
+	[CreditLimit] [decimal](18, 2) NULL,
+	[AccountOpenedDate] [date] NOT NULL,
+	[StandardDiscountPercentage] [decimal](18, 3) NOT NULL,
+	[IsStatementSent] [bit] NOT NULL,
+	[IsOnCreditHold] [bit] NOT NULL,
+	[PaymentDays] [int] NOT NULL,
+	[PhoneNumber] [nvarchar](20) NOT NULL,
+	[FaxNumber] [nvarchar](20) NOT NULL,
+	[DeliveryRun] [nvarchar](5) NULL,
+	[RunPosition] [nvarchar](5) NULL,
+	[WebsiteURL] [nvarchar](256) NOT NULL,
+	[DeliveryAddressLine1] [nvarchar](60) NOT NULL,
+	[DeliveryAddressLine2] [nvarchar](60) NULL,
+	[DeliveryPostalCode] [nvarchar](10) NOT NULL,
+	[DeliveryLocation] [geography] NULL,
+	[PostalAddressLine1] [nvarchar](60) NOT NULL,
+	[PostalAddressLine2] [nvarchar](60) NULL,
+	[PostalPostalCode] [nvarchar](10) NOT NULL,
+	[LastEditedBy] [int] NOT NULL,
+	[ValidFrom] [datetime2](7) NOT NULL,
+	[ValidTo] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_Sales_Customers] PRIMARY KEY CLUSTERED  ([CustomerID] ASC)
+ )
+ GO
+
+
